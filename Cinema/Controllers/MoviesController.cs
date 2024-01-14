41421d84d6333796b59f585219525fcdad9b0e43
kuqit <kuqit@LAPTOP-ZAN>
2024-01-14 16:27:05 +0200

    Add project files.

---
+++ Cinema/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cinema.Models;
+
+namespace Cinema.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+        }
+    }
+}
